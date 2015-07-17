0f1d6eeaff904440102dfe1d5002c554fbfa0462
lekaf <tony.seperoumal@gmail.com>
2015-07-17 10:53:42 +0200

    add contents and some headers

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Brocoli</title>
 </head>
 <body>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, cum, incidunt atque deserunt pariatur voluptate obcaecati recusandae eaque error ut possimus doloremque vel quo dolore ducimus porro harum culpa unde!</p>
 	
 </body>
 </html>
