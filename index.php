279a3d91a37e2417eef17074d19b1e0bac53c449
lekaf <tony.seperoumal@gmail.com>
2015-07-17 11:09:20 +0200

    add main stylesheet

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Brocoli</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, cum, incidunt atque deserunt pariatur voluptate obcaecati recusandae eaque error ut possimus doloremque vel quo dolore ducimus porro harum culpa unde!</p>
